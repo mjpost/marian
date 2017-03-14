f14cd0425cb904eeb2507f803892e81be881a070
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2017-03-14 17:03:32 +0000

    Add multi layer conv

--- src/layers/convolution.h
+++ src/layers/convolution.h
@@ -6,26 +6,47 @@
 
 namespace marian {
 
-
 class Convolution : public Layer {
   public:
     Convolution(const std::string& name)
       : Layer(name) {
     }
 
-    Expr operator()(Expr in, int kernelNum, int kernelHeight, int kernelWidth, int kernelHPad, int kernelWPad,
-        int poolingHeight, int poolingWidth, int poolingHPad, int poolingWPad) {
+    Expr operator()(Expr in, int kernelNum, int kernelHeight, int kernelWidth, int kernelHPad, int kernelWPad) {
       auto graph = in->graph();
       auto kernels = graph->param(name_ + "kernels", {kernelNum, 1, kernelHeight, kernelWidth},
           keywords::init=inits::from_value(1.0f));
-          // keywords::init=inits::glorot_uniform);
 
       params_ = { kernels };
 
       auto conv = convolution(in, kernels, kernelHPad, kernelWPad);
-      auto pooling = max_pooling(conv, poolingHPad, poolingWPad, poolingHeight, poolingWidth);
-      return pooling;
+      return conv;
     }
+};
+
+
+class MultiConvLayer : public Layer {
+  public:
+    MultiConvLayer(const std::string& name, int stackDim)
+      : Layer(name),
+        stackDim_(stackDim)
+    {}
+
+
+  Expr operator()(Expr x, Expr xMask) {
+    auto masked = x * xMask;
+    auto graph = x->graph();
+    Expr* in = &masked;
+    for (int idx = 0; idx < stackDim_; ++idx) {
+      auto out = tanh(*in + Convolution(name_ + std::to_string(idx))(*in, 1, 3, 1, 1, 0));
+      in = &out;
+    }
+
+    return *in;
+  }
+
+  protected:
+    int stackDim_;
 };
 
 }
